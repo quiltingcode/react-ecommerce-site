b18236e03ec9b132adfa01aeaebf9d0171b03c75
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-31 10:30:32 +0000

    Add routes and outlets for home and shop

--- clothes-store/src/App.js
+++ clothes-store/src/App.js
@@ -1,10 +1,28 @@
 import Home from "./routes/home/home.component"
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navbar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
