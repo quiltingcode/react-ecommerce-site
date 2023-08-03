3ff944d4b2444ef43979fe7bd3a948c45d10ff01
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-03 12:04:59 +0000

    Update firestore db params

--- clothes-store/src/utils/firebase/firebase.utils.js
+++ clothes-store/src/utils/firebase/firebase.utils.js
@@ -5,6 +5,12 @@ import {
   signInWithPopup, 
   GoogleAuthProvider, 
 } from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDmDKbtGSUVm_gZBQCC6EARRmhs9X4Clys",
@@ -25,3 +31,14 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists())
+}
