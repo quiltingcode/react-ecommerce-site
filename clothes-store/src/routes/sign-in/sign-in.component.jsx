25f2a2c0b71ecfc21b47d8732f905308c12d6776
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-08 15:28:25 +0000

    feat: Create signup form component

--- clothes-store/src/routes/sign-in/sign-in.component.jsx
+++ clothes-store/src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,9 @@
-import { useEffect } from "react";
-import { getRedirectResult } from 'firebase/auth';
-
 import { 
-    auth,
     signInWithGooglePopup, 
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect, 
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-
-    useEffect(async () => {
-        const response = await getRedirectResult(auth);
-        if(response) {
-            const userDocRef = await createUserDocumentFromAuth(response.user)
-        }
-    }, [])
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
@@ -27,9 +15,6 @@ const SignIn = () => {
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>
                 Sign in with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
-                Sign in with Google Redirect
             </button>
         </div>
     );
