ff8425213b87b807198487af2b383a085a8f1463
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-12 13:28:53 +0000

    feat: Add signup authentication

--- clothes-store/src/components/sign-up/sign-up.component.jsx
+++ clothes-store/src/components/sign-up/sign-up.component.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
     displayName: '',
@@ -13,11 +13,33 @@ const SignUpForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {displayName, email, password, confirmPassword} = formFields;
 
-    console.log(formFields)
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
 
     const handleSubmit = async (event) => {
-
-    }
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            alert("passwords do not match");
+            return;
+        }
+        
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(
+                email, 
+                password
+            );
+            
+            await createUserDocumentFromAuth(user, { displayName } );
+            resetFormFields();
+        } catch (error) {
+            if(error.code === 'auth/email-already-in-use') {
+                alert('Cannot create user, email already in use');
+            } else {
+                console.log('user creation encoutered an error', error);
+            }
+        }
+    };
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -27,7 +49,7 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Signup with your email and password</h1>
-            <form onSubmit={() => {}}>
+            <form onSubmit={handleSubmit}>
                 <label>Display Name</label>
                 <input 
                     type="text" 
--- clothes-store/src/utils/firebase/firebase.utils.js
+++ clothes-store/src/utils/firebase/firebase.utils.js
@@ -36,7 +36,10 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (
+  userAuth, 
+  additionalInformation = {}
+) => {
   if (!userAuth) return;
   
   const userDocRef = doc(db, 'users', userAuth.uid);
@@ -52,6 +55,7 @@ export const createUserDocumentFromAuth = async (userAuth) => {
         displayName,
         email,
         createdAt,
+        ...additionalInformation,
       });
     } catch (error) {
       console.log('error creating the error', error.message);
