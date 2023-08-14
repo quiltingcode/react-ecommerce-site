38e7b99e50925a16c69ecbfd90f8805593fd8889
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-14 10:17:49 +0000

    feat: Add reusable button component

--- clothes-store/src/components/sign-up/sign-up.component.jsx
+++ clothes-store/src/components/sign-up/sign-up.component.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input-component";
 import './sign-up-form.styles.scss';
+import Button from "../button/button.component";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const defaultFormFields = {
@@ -89,7 +90,7 @@ const SignUpForm = () => {
                     name="confirmPassword"
                     value={confirmPassword}
                 />
-                <button type="submit">Sign Up</button>
+                <Button type="submit">Sign Up</Button>
             </form>
         </div>
     )
