355543a0c6da36bae8d8bac220d3c40b81826356
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-15 17:10:25 +0000

    bug: fix google popup signin

--- clothes-store/src/routes/authentication/authentication.component.jsx
+++ clothes-store/src/routes/authentication/authentication.component.jsx
@@ -1,23 +1,18 @@
-import { 
-    signInWithGooglePopup, 
-    createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
-import SignUpForm from "../../components/sign-up/sign-up.component";
-import SignInForm from "../../components/sign-in/sign-in-form.component";
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 
 const Authentication = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    };
+    }
 
     return (
         <div>
             <h1>Sign In Page</h1>
-            
-            <SignInForm />
-            <SignUpForm />
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
         </div>
     );
 };
