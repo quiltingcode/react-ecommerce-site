0fdc14c0c1e0d4b1e91bf467aad7221e314ac0ea
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-17 11:35:49 +0000

    feat: Create user Context

--- clothes-store/src/index.js
+++ clothes-store/src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import './index.scss';
+import { render } from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import {StrictMode} from 'react';
-import { render } from 'react-dom';
+import { UserProvider } from './contexts/user.context';
+
+import './index.scss';
 
 const rootElement = document.getElementById('root');
 
 render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </StrictMode>,
   rootElement
