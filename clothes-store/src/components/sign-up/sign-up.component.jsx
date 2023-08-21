d0228feb8e1169a7afe11a23b43f0099d968773a
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-08-21 09:51:58 +0000

    feat: sign out method

--- clothes-store/src/components/sign-up/sign-up.component.jsx
+++ clothes-store/src/components/sign-up/sign-up.component.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import FormInput from "../form-input/form-input-component";
 import './sign-up-form.styles.scss';
 import Button from "../button/button.component";
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import { UserContext } from "../../contexts/user.context";
 
 const defaultFormFields = {
     displayName: '',
@@ -15,6 +17,8 @@ const SignUpForm = () => {
 
     const [formFields, setFormFields] = useState(defaultFormFields);
     const {displayName, email, password, confirmPassword} = formFields;
+
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
@@ -32,6 +36,8 @@ const SignUpForm = () => {
                 email, 
                 password
             );
+
+            setCurrentUser(user);
             
             await createUserDocumentFromAuth(user, { displayName } );
             resetFormFields();
--- clothes-store/src/routes/navigation/navigation.component.jsx
+++ clothes-store/src/routes/navigation/navigation.component.jsx
@@ -2,24 +2,34 @@ import { Outlet, Link } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  console.log(currentUser)
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    const res = await signOutUser();
+    setCurrentUser(null);
+  };
+
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrownLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrownLogo className="logo" />
         </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop' >
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth' >
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
