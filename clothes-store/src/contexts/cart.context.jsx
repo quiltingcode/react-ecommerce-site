1917415d6bebc5475b9b42586ab7385f1ac146c5
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-11-23 16:06:13 +0000

    Fix button component, create add item to cart function

--- clothes-store/src/contexts/cart.context.jsx
+++ clothes-store/src/contexts/cart.context.jsx
@@ -1,14 +1,40 @@
 import { createContext, useState } from "react";
 
+export const addCartItem = (cartItems, productToAdd) => {
+    // find if cartItems container productToAdd
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id
+    );
+
+    // If found, imcrement quantity
+    if(existingCartItem) {
+        return cartItems.map((cartItem) => 
+            cartItem.id === productToAdd.id
+                ? {...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
+
+    // return new array with modified cartItems/new cart item
+    return [...cartItems, {...productToAdd, quantity: 1}];
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}
+    setIsCartOpen: () => {},
+    cartItems: [],
+    addItemToCart: () => {}
 })
 
 export const CartProvider = ({children}) => {
     
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (product) => {
+        setCartItems(addCartItem(cartItems, product));
+    }
+
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
     
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
     
